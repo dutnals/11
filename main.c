28b07d99a624b128e2f8f42c79a68904660c1e78
dutnals <dutnaks77@sookmyung.ac.kr>
2025-11-13 15:22:48 +0900

    02

--- main.c
+++ main.c
@@ -1,21 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(void) { 
-    int i = 10; 
-    char c = 'a';
-	
-	int *iptr;
-	char *cptr;
-	int *iptr2;
-	
-	iptr = &i;
-	cptr = &c;
-	iptr2 = iptr;
+void main(void) {
+    int i = 300;  
+    int *pi = &i;
+    char *pc = &i;
 
-    printf("i : %p\n%p (size:%i)\n", iptr, &i, sizeof(iptr)); 
-    printf("c : %p\n%p (size:%i)\n", cptr, &c, sizeof(cptr)); 
-    printf("iptr2 : %p, %i\n", iptr2, *iptr2); 
-    return 0; 
+    printf("%i, %i, %i\n", i, *pi, *pc);
 }
-
